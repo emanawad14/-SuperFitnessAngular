4082f1f7ceed5781106642765d287aed5a9306d2
emanawad14 <emanelnady2001@gmail.com>
2025-11-14 14:41:06 +0200

    UpdatedProject

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes =
+ [
+
+     {path:'',loadComponent:()=>import('./features/home/home.component').then((c)=>c.HomeComponent)
+        ,title:'Home'
+    },
+    {path:'login',loadComponent:()=>import('./core/auth/login/login.component').then((c)=>c.LoginComponent)
+        ,title:'Login'
+    },
+    
+    {path:'register',loadComponent:()=>import('./core/auth/register/register.component').
+        then((c)=>c.RegisterComponent)
+        ,title:'Register'
+    }
+
+
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlowbiteService } from './core/services/flowbite.service';
+import { initFlowbite } from 'flowbite';
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App  implements OnInit{
   protected readonly title = signal('superFitness');
+
+  constructor(private flowbiteService: FlowbiteService) {}
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
 }
