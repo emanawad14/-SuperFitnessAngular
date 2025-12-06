aca3ef29c007e395af1a3e0b72ad8ad7e4703162
emanawad14 <emanelnady2001@gmail.com>
2025-12-07 01:39:25 +0200

    UpdateClasses

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,24 @@ export const routes: Routes =
      {path:'',loadComponent:()=>import('./features/home/home.component').then((c)=>c.HomeComponent)
         ,title:'Home'
     },
+
+     {path:'about',loadComponent:()=>import('./features/about/about.component').
+        then((c)=>c.AboutComponent)
+        ,title:'About'
+    },
+    {path:'classes',loadComponent:()=>import('./features/workouts/workouts.component').
+        then((c)=>c.WorkoutsComponent)
+        ,title:'Classes'
+    },
+    {path:'healthy',loadComponent:()=>import('./features/healthy/healthy.component').
+        then((c)=>c.HealthyComponent)
+        ,title:'Healthy'
+    },
+     {path:'classId/:id',loadComponent:()=>import('./features/classes/classes.component').
+        then((c)=>c.ClassesComponent)
+        ,title:'classId'
+    },
+
     {path:'login',loadComponent:()=>import('./core/auth/login/login.component').then((c)=>c.LoginComponent)
         ,title:'Login'
     },
@@ -13,7 +31,12 @@ export const routes: Routes =
     {path:'register',loadComponent:()=>import('./core/auth/register/register.component').
         then((c)=>c.RegisterComponent)
         ,title:'Register'
-    }
+    },
+   
+   
+    
+   
+    
 
 
 
